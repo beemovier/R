ffe70b34d8ac974676871afb1ad6444472d7e987
BeeVe <erick129ferreira@gmail.com>
2025-04-04 19:58:26 -0300

    Update parouimpar4.r

--- parouimpar4.r
+++ parouimpar4.r
@@ -1,19 +1,35 @@
 jogo_parouimpar <- function(a) {
   # Verifica se 'a' é numérico, maior que 0 e menor ou igual a 10
   if (class(a)=="numeric"&& a <=10 && a>0) {
-  set.seed(5)
+
     escolha<- c(1:10)
-    bot<-sample(escolha,1)
-    cat("Sua escolha: ", a,"\n")
-    cat("Escolha do bot: ", bot,"\n")
-    calc<-(a+bot)%2
-    for(i in 1:3){
+    
+    #faz duas vezes a escolha para deixar mais aleatório
+    aleatorio<-sample(escolha,5)
+    bot<-sample(aleatorio,1)
+    cat("Sua escolha: ", a," é ")
+    if(a%%2==0){cat("par","\n")}else{cat("impar\n")}
+      for(i in 1:3){
     flush.console()
-    Sys.sleep(1)
+    Sys.sleep(0.5)
         cat(".")
     }
-    if(calc==par){
+    flush.console()
+    Sys.sleep(0.5)
+     cat("\n")
+    cat("Escolha do bot: ", bot," é ")
+    if(bot%%2==0){cat("par","\n")}else{cat("impar\n")}
+    calc<-a+bot
+    flush.console()
+    Sys.sleep(0.2)
+     # cat("\n")
+    parimpar<-calc%%2
+    cat("Resultado: ")
+    if(parimpar==0){
+    print("PAR")
         
+    }else{
+    print("IMPAR")
     }
     
     
